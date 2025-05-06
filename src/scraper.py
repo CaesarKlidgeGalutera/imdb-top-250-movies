02c3eb7ab3c420d20561c56684b70e19040b6c39
Caesar Klidge Galutera <caesarklidgegalutera@gmail.com>
2025-05-06 21:57:06 +0800

    Change Script Source. Movie details are now scraped.

--- src/scraper.py
+++ src/scraper.py
@@ -22,24 +22,39 @@ try:
     soup = BeautifulSoup(response.text, 'html.parser')
     
     #Looks for script tag with JSON-LD that contains the movie data
-    scripts = soup.find("script", type="application/ld+json")
+    scripts = soup.find("script", id="__NEXT_DATA__", type="application/json")
     
     #parse it as JSON
-    data = json.loads(scripts.string)
-
-    if isinstance(data, dict) and "itemListElement" in data:
-        movies_data = [
-            { 
-                "title":movie["item"].get("name","N/A")
-            }
+    if scripts:
+        try:
+            data = json.loads(scripts.string)
+            edges = data["props"]["pageProps"]["pageData"]["chartTitles"]["edges"]
             
-            for movie in data["itemListElement"]
-        ]
+            movies_data = [
+                {
+                    "rank":movie["currentRank"],
+                    "title":movie["node"]["titleText"]["text"],
+                    "release_date": f'{movie["node"].get("releaseDate").get("day","??")}-{movie["node"].get("releaseDate").get("month","N/A")}-{movie["node"].get("releaseDate").get("year","N/A")}',
+                    "genre":[g["genre"]["text"] for g in movie["node"].get("titleGenres", {}).get("genres", [])],
+                    "movie_rating": (movie["node"].get("certificate") or {}).get("rating", "N/A"),
+                    "rating_summary":movie["node"].get("ratingsSummary", {}).get("aggregateRating", "N/A"),
+                    "vote_count":movie["node"].get("ratingsSummary", {}).get("voteCount", "N/A"),
+                    "run_time_seconds":movie["node"].get("runtime",{}).get("seconds","N/A")
+                }
+                    
+                for movie in edges
+            ]
+            
 
-    with open("../data/raw/data.json","w") as f:
-        json.dump(movies_data, f, indent=4,ensure_ascii=False)   
-        
-    print("Saved movies to raw folder ✅")
+            with open("../data/raw/data.json","w") as f:
+                json.dump(movies_data, f, indent=4,ensure_ascii=False)  
+                
+            print("Saved movies to raw folder ✅")
+              
+        except KeyError as e:
+            print(f"Key error in JSON structure: {e}")
+
+    
 # return if error  
 except requests.exceptions.RequestException as e:
     print(f"Failed to fetch the page. Error: {e}")
