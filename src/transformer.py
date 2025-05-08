155185cc9936b3d8fe0c9ba7f06d28be6bdc356a
Caesar Klidge Galutera <caesarklidgegalutera@gmail.com>
2025-05-08 21:40:24 +0800

    Add datetime transformation

--- src/transformer.py
+++ src/transformer.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 
 file_path = '../data/raw/data.json'
 
@@ -15,11 +16,16 @@ try:
         remaining = movie['run_time_seconds'] % 3600
         minutes = remaining / 60
         movie['run_time_seconds'] = f"{int(hours)}h {int(minutes)}m"
+    
+    date = datetime.strptime(data["scraped_at"], "%Y-%m-%dT%H:%M:%S.%f" )  
+    data["scraped_at"] = date.strftime("%m/%d/%Y-%H:%M:%S")
 
 
     with open("../data/cleaned/cleaned.json", 'w') as file:
         json.dump(data, file, indent=4)
         
+    print("✅ Successfully Transformed json file. Saved at data/cleaned folder")
+        
 
 except KeyError as e: #raises an error if keys value pairs are wrong
-    print(f"Key error in JSON structure: {e}")
+    print(f"❌ Key error in JSON structure: {e}")
