6c32a9478e86085f8afce2614df598f012711e94
Caesar Klidge Galutera <caesarklidgegalutera@gmail.com>
2025-05-08 21:40:53 +0800

    Fixed Uploading bugs

--- src/uploader.py
+++ src/uploader.py
@@ -9,16 +9,18 @@ s3 = boto3.client('s3')
 
 bucket_name = os.getenv('bucket_name')
 
-file_path = '../data/raw/data.json'
+file_path = '../data/cleaned/cleaned.json'
 
 with open(file_path, 'r') as file:
     data = json.load(file)
     
-file_name = f"imdb-top-250-movies-{data["scraped_at"]}"
+file_name = "imdb-top-250-movies"
 
 s3.put_object(
     Bucket=bucket_name,
     Key=file_name,
-    Body=file_path,
+    Body= json.dumps(data) ,
     ContentType='application/json'
 )
+
+print("✅ JSON File is successfully uploaded in S3")
