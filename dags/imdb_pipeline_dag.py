7e70271323b50371c8c0434223dd9763210ea306
Caesar Klidge Galutera <caesarklidgegalutera@gmail.com>
2025-05-22 23:27:06 +0800

    test airflow

--- dags/imdb_pipeline_dag.py
+++ dags/imdb_pipeline_dag.py
@@ -1,4 +1,15 @@
 from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+
+def test():
+    print("Test")
+    
+p_upload = PythonOperator(
+    task_id = 'test',
+    python_callable= test,
+    op_kwargs = {}
+)
 
 
 upload = BashOperator(
