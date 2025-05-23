0fc3cf7659cb359feb2445b0a27b30b3cb171b1f
Caesar Klidge Galutera <caesarklidgegalutera@gmail.com>
2025-05-23 23:54:12 +0800

    tested airflow scheduler

--- dags/imdb_pipeline_dag.py
+++ dags/imdb_pipeline_dag.py
@@ -1,6 +1,14 @@
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
+from airflow import DAG
+from datetime import datetime
 
+default_args = {
+    'owner':'klidge',
+    'start_date' : datetime(2025, 5, 23)
+}
+
+imdb_top_250_movies = DAG(dag_id ='top_250_movies', default_args=default_args, schedule_interval= '10 0 * * *') #runs daily at 10am
 
 def test():
     print("Test")
@@ -9,14 +17,16 @@ p_upload = PythonOperator(
     task_id = 'test',
     python_callable= test,
     op_kwargs = {}
+    dag=imdb_top_250_movies
 )
 
 
 upload = BashOperator(
     task_id='scrape_data',
-    bash_command='scraper.py'
+    bash_command='scraper.py',
+    dag=p_upload
 )
 
 
-upload
+upload >> p_upload
 
