bab02fe4cf91c115cee0cda1425dace99ea3667f
Caesar Klidge Galutera <caesarklidgegalutera@gmail.com>
2025-05-27 23:51:52 +0800

    edited workflows and tasks

--- dags/imdb_pipeline_dag.py
+++ dags/imdb_pipeline_dag.py
@@ -1,32 +1,48 @@
 from airflow.operators.bash import BashOperator
-from airflow.operators.python import PythonOperator
+from airflow.sensors.filesystem import FileSensor
 from airflow import DAG
-from datetime import datetime
+from datetime import datetime, timedelta
 
 default_args = {
     'owner':'klidge',
-    'start_date' : datetime(2025, 5, 23)
+    'start_date' : datetime(2025, 5, 23),
+    'sla':timedelta(minutes=30),
+    'email': 'caesarklidge@gmail.com',
+    'email_on_failure': True,
+    'email_on_sucess':True
 }
 
-imdb_top_250_movies = DAG(dag_id ='top_250_movies', default_args=default_args, schedule_interval= '10 0 * * *') #runs daily at 10am
+imdb_top_250_movies = DAG(
+    dag_id ='top_250_movies',
+    default_args=default_args,
+    schedule_interval= '10 0 * * *') #runs daily at 10am
 
-def test():
-    print("Test")
-    
-p_upload = PythonOperator(
-    task_id = 'test',
-    python_callable= test,
-    op_kwargs = {}
+
+scrape = BashOperator(
+    task_id = 'scrape_date',
+    bash_command='scraper.py',
+    start_date= datetime(2025, 5, 30),
     dag=imdb_top_250_movies
 )
 
+check_raw_data = FileSensor(
+    task_id ='check_raw_data',
+    filepath= 'C:\Users\caesa\Documents\GitHub\imdb-top-250-movies\data\data.json',
+    start_date= datetime(2025, 5, 30),
+    mode='reschedule',
+    dag=imdb_top_250_movies)
 
-upload = BashOperator(
-    task_id='scrape_data',
-    bash_command='scraper.py',
-    dag=p_upload
+transform = BashOperator(
+    task_id= 'transform_raw_data',
+    bash_command='transformer.py',
+    start_date= datetime(2025, 5, 30),
+    dag=imdb_top_250_movies
+)
+
+check_cleaned_data= BashOperator(
+    
 )
 
 
-upload >> p_upload
+scrape >> check_raw_data >> transform
 
